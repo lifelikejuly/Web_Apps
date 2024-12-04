718293cd6596b097e3631fa17fd66399b0df7bec
yuhaocan <yuhaocan@weidian.com>
2024-12-04 14:23:11 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   app:{
-     baseURL: '/apps/',
+     baseURL: '/Web_Apps/',
     //  buildAssetsDir: 'nuxt_assets',
     
   },
